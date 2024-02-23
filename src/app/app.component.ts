6de0b81ffc850dec290406240f12fcfaa39df026
Tchantcho-tchoutto-isaac <94875081+Tchantcho-tchoutto-isaac@users.noreply.github.com>
2024-02-23 15:36:35 +0100

    ajout du model Pokemon et des pokemons mock-pokemon-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  Pokemons = ['bulbizarre','salamèche','carapuce'];
+export class AppComponent implements OnInit {
+  PokemonList = ['bulbizarre','salamèche','carapuce'];
+
+  ngOnInit(): void {
+      console.table(this.PokemonList);
+      this.selectpokemon('salameche');
+  }
+
+   selectpokemon(pokemonName:string){
+    console.log(`Vous avez cliqué sur le Pokémon ${pokemonName}`);
+  }
 }

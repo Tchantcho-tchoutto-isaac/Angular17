84539a63d254a5e0ddde1ec07c749faff3d0381d
Tchantcho-tchoutto-isaac <94875081+Tchantcho-tchoutto-isaac@users.noreply.github.com>
2024-02-24 19:14:53 +0100

    selectPokemon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,33 @@
 import { Component,OnInit } from '@angular/core';
-
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  PokemonList = ['bulbizarre','salamèche','carapuce'];
+  PokemonList:Pokemon[]=POKEMONS;
+  pokemonselectted:Pokemon|undefined;
+  
 
   ngOnInit(): void {
       console.table(this.PokemonList);
-      this.selectpokemon('salameche');
+
   }
 
-   selectpokemon(pokemonName:string){
-    console.log(`Vous avez cliqué sur le Pokémon ${pokemonName}`);
+   selectpokemon(PokemonId:string){
+
+    const pokemon:Pokemon|undefined=this.PokemonList.find(pokemon=>pokemon.id==+pokemon)
+    if(pokemon){
+      console.log(`Vous avez cliqué sur le Pokémon ${pokemon.name}`)
+      this.pokemonselectted=pokemon
+    }
+    else{
+      console.log(`vous avez demandé un pokemon qui n'existe pas .`)
+      this.pokemonselectted=pokemon;
+      
+    }
+    ;
   }
 }

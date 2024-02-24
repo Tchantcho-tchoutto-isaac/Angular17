148cca0622df36c34e1951fef10ab53fb9ceb768
Tchantcho-tchoutto-isaac <94875081+Tchantcho-tchoutto-isaac@users.noreply.github.com>
2024-02-24 23:20:24 +0100

    Mise en place du pipe pokemon-type-color terminée

--- src/app/pokemon-type-color.pipe.ts
+++ src/app/pokemon-type-color.pipe.ts
@@ -5,8 +5,37 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PokemonTypeColorPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(type: string): string {
+    let color: string;
+    switch (type) {
+      case 'Feu':
+        color = 'red lighten-1';
+        break;
+      case 'Eau':
+        color = 'blue lighten-1';
+        break;
+      case 'Plante':
+        color = 'green lighten-1';
+        break;
+      case 'Insecte':
+        color = 'brown lighten-1';
+        break;
+      case 'Normal':
+        color = 'grey lighten-1';
+        break;
+      case 'Vol':
+        color = 'blue lighten-1';
+        break;
+      case 'Poisson':
+        color = 'deep-purple accent-1';
+        break;
+      case 'Fée':
+        color = 'pink lighten-4';
+        break;
+      default:
+        color = 'grey';
+        break;
+    }
+    return color;
   }
-
 }

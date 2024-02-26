b2d5bc7302dc2b8a0a37ca138db247c0048956a5
Tchantcho-tchoutto-isaac <94875081+Tchantcho-tchoutto-isaac@users.noreply.github.com>
2024-02-26 18:05:28 +0100

    pokemon detail

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from '../pokemon';
+import { ActivatedRoute } from '@angular/router';
+import { POKEMONS } from '../mock-pokemon-list';
 
 @Component({
   selector: 'app-detail-pokemon',
   templateUrl: './detail-pokemon.component.html',
   styleUrl: './detail-pokemon.component.css'
 })
-export class DetailPokemonComponent {
+export class DetailPokemonComponent  implements OnInit {
+  pokemonList:Pokemon[];
+  pokemon:Pokemon|undefined;
+
+  constructor(private route:ActivatedRoute){}
+  
+  
+  ngOnInit() { // Ajout de la méthode ngOnInit
+    this.pokemonList = POKEMONS;
+    const pokemonId: string | null = this.route.snapshot.paramMap.get('id');
+
+    if (pokemonId) {
+      this.pokemon = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
+    }
+  }
 
 }

0fc6c84f3b180bc4edf574f4cd09d97f21395609
Tchantcho-tchoutto-isaac <94875081+Tchantcho-tchoutto-isaac@users.noreply.github.com>
2024-02-27 17:24:15 +0100

    organisations de mon module pokemon

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
+import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
+import { BorderCardDirective } from './border-card.directive';
+import { PokemonTypeColorPipe } from './pokemon-type-color.pipe'; 
 
 
 
+const pokemonRoutes:Routes=[
+  {path:'pokemons', component:ListPokemonComponent},
+  {path:'pokemon/:id', component:DetailPokemonComponent},
+]
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListPokemonComponent,
+    PokemonTypeColorPipe,
+    DetailPokemonComponent,
+    BorderCardDirective,
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(pokemonRoutes)
+    
   ]
 })
 export class PokemonModule { }

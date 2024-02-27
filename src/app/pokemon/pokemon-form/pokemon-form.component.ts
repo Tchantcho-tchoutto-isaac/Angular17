85d02f15c40a79df69be13f9405994216be36d26
Tchantcho-tchoutto-isaac <94875081+Tchantcho-tchoutto-isaac@users.noreply.github.com>
2024-02-27 23:47:59 +0100

    modifications du template du pokemon-form

--- src/app/pokemon/pokemon-form/pokemon-form.component.ts
+++ src/app/pokemon/pokemon-form/pokemon-form.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, input } from '@angular/core';
+import { PokemonService } from '../pokemon.service';
+import { Pokemon } from '../pokemon';
 
 @Component({
   selector: 'app-pokemon-form',
   templateUrl: './pokemon-form.component.html',
   styleUrl: './pokemon-form.component.css'
 })
-export class PokemonFormComponent {
+export class PokemonFormComponent implements OnInit {
+@Input() Pokemon:Pokemon;
+type:string[];
 
+  constructor(private pokemonservice:PokemonService){
+
+  }
+
+  ngOnInit() {
+    this.type=this.pokemonservice.getPokemonTypeList();
+      
+  }
+ hasType(type:string):boolean{
+  return this.Pokemon.types.includes(type);
+ }
+selectType(){
+
+}
+onsubmit(){
+
+}
 }

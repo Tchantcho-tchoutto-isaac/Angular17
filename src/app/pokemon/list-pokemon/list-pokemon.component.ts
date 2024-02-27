2eea7ad9761bb8f34dd60634ac81ca38b3f675c4
Tchantcho-tchoutto-isaac <94875081+Tchantcho-tchoutto-isaac@users.noreply.github.com>
2024-02-27 18:03:14 +0100

    ajout du service Pokemon dans list-pokemon.component .ts

--- src/app/pokemon/list-pokemon/list-pokemon.component.ts
+++ src/app/pokemon/list-pokemon/list-pokemon.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Pokemon } from '../pokemon';
 import { POKEMONS } from '../mock-pokemon-list';
 import { Router } from '@angular/router';
+import { PokemonService } from '../pokemon.service';
 POKEMONS
 Pokemon
 @Component({
@@ -9,11 +10,14 @@ Pokemon
   templateUrl: './list-pokemon.component.html',
   styleUrl: './list-pokemon.component.css'
 })
-export class ListPokemonComponent {
-  PokemonList:Pokemon[]=POKEMONS;
-  constructor(private router :Router){
+export class ListPokemonComponent implements OnInit {
+  PokemonList:Pokemon[];
+  constructor(private router :Router,private PokemonService:PokemonService){
 
   }
+    ngOnInit(): void {
+        this.PokemonList=this.PokemonService.getPokemon();
+    }
   goToPokemon(pokemon: Pokemon){
     this.router.navigate(['/pokemon', pokemon.id]);
   }

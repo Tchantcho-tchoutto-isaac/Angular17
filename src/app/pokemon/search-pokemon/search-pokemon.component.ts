334e9707cad7ef597c038f010e3b1ab21e48c9eb
Tchantcho-tchoutto-isaac <94875081+Tchantcho-tchoutto-isaac@users.noreply.github.com>
2024-03-12 23:12:26 +0100

    ajout du champ de recherche dans le template list-pokemon.component

--- src/app/pokemon/search-pokemon/search-pokemon.component.ts
+++ src/app/pokemon/search-pokemon/search-pokemon.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from '../pokemon';
+import { Router } from '@angular/router';
+import { Observable, Subject } from 'rxjs';
+
 
 @Component({
   selector: 'app-search-pokemon',
   templateUrl: './search-pokemon.component.html',
   styleUrl: './search-pokemon.component.css'
 })
-export class SearchPokemonComponent {
+export class SearchPokemonComponent  implements OnInit{
+ seachTerms=new Subject<string>();
+ pokemons$:Observable<Pokemon[]>; 
+  constructor(private router:Router){}
+  ngOnInit(): void {
+     
+  }
+  search(term: string){
 
+  }
+   goToDetail(pokemon: Pokemon){
+
+    const link =['/pokemon',pokemon.id];
+    this.router.navigate(link);
+   }
 }

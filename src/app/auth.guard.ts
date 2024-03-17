7ece900a761409c648b8198978cd57f89b6aaf92
Tchantcho-tchoutto-isaac <94875081+Tchantcho-tchoutto-isaac@users.noreply.github.com>
2024-03-17 22:59:01 +0100

    amelioration de notre guard

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
-@Injectable({
-  providedIn:'root'
-})
 
+@Injectable({
+  providedIn: 'root'
+})
 export class AuthGuard implements CanActivate {
   
-  CanActivate(){
-    console.log('le guard a bien été appelé');
+  canActivate() {
+    console.log('Le garde a bien été appelé');
     return true;
-
   }
   
-};
+}
+
--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -12,12 +12,12 @@ import { EditPokemonComponent } from './edit-pokemon/edit-pokemon.component';
 import { AddPokemonComponent } from './add-pokemon/add-pokemon.component';
 import { SearchPokemonComponent } from './search-pokemon/search-pokemon.component';
 import { LoaderComponent } from './loader/loader.component';
-import { authGuard } from '../auth.guard';
+import { AuthGuard } from '../auth.guard';
  
 
 
 const pokemonRoutes:Routes=[
-  {path:'edit/pokemon/:id', component:EditPokemonComponent ,canActivate:[authGuard]},
+  {path:'edit/pokemon/:id', component:EditPokemonComponent ,canActivate:[AuthGuard]},
   {path:'pokemon/add',component:AddPokemonComponent},
   {path:'pokemons', component:ListPokemonComponent},
   {path:'pokemon/:id', component:DetailPokemonComponent},

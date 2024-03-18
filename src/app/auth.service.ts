6613e3801b38d42c6d63f3ebffc61c0d9ed3b3a6
Tchantcho-tchoutto-isaac <94875081+Tchantcho-tchoutto-isaac@users.noreply.github.com>
2024-03-18 01:33:09 +0100

    mise en place de la methode Loggin dans le service auth.service.ts

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Observable, delay, of,tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+isLoggedIn:boolean=false;
+redirectUrl:string;
+Login(name:string,password:string):Observable<boolean>{
+  const isLoggedIn=(name=='pikachu'&& password =='pikachu');
+
+
+  return of(isLoggedIn).pipe(delay(1000),
+  tap(isLoggedIn=>this.isLoggedIn=isLoggedIn));
+}
+
+logout(){
+
+}
 }
